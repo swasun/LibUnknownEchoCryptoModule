faa0130225f0173e0d2075440322672e7528accf
Charly Lamothe <charly.lamothe@sfr.fr>
2018-07-08 21:21:45 +0200

    no-tests for openssl isn't known

--- external/openssl.cmake
+++ external/openssl.cmake
@@ -23,7 +23,7 @@ if (UNIX)
     ExternalProject_Add(openssl
         PREFIX openssl
         URL http://www.openssl.org/source/openssl-1.1.0h.tar.gz
-        CONFIGURE_COMMAND ./config no-tests no-shared no-idea no-mdc2 no-rc4 no-rc5 --prefix=${CMAKE_BINARY_DIR}
+        CONFIGURE_COMMAND ./config no-shared no-idea no-mdc2 no-rc5 --prefix=${CMAKE_BINARY_DIR}
         BUILD_COMMAND make depend && make
         INSTALL_COMMAND make install_sw
         BUILD_IN_SOURCE 1
