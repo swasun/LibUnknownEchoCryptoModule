48f19eab2d24d206aa85ad2c4f3889174a35bde5
Charly Lamothe <charly.lamothe@sfr.fr>
2018-07-09 13:09:23 +0200

    Trying to prevent build openssl multiple time when dependency of libuecm have to build openssl

--- external/openssl.cmake
+++ external/openssl.cmake
@@ -19,39 +19,35 @@
 
 set(OPENSSL_TARGET_SET false)
 
-message(STATUS "CMAKE_CURRENT_BINARY_DIR: " ${CMAKE_CURRENT_BINARY_DIR})
-
 if (EXISTS ${CMAKE_CURRENT_BINARY_DIR}/openssl/src/openssl/libcrypto.a)
-    message(STATUS "TRUE")
     set(OPENSSL_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl/src/openssl/)
     set(OPENSSL_LIBRARIES
         ${CMAKE_CURRENT_BINARY_DIR}/openssl/src/openssl/libcrypto.a
         ${CMAKE_CURRENT_BINARY_DIR}/openssl/src/openssl/libssl.a
     )
 else ()
-message(STATUS "FALSE")
     include (ExternalProject)
 
     if (UNIX)
         ExternalProject_Add(openssl
-            PREFIX openssl
+            PREFIX ${CMAKE_CURRENT_BINARY_DIR}
             URL http://www.openssl.org/source/openssl-1.1.0h.tar.gz
             CONFIGURE_COMMAND ./config no-crypto-mdebug no-shared
                 no-crypto-mdebug-backtrace no-unit-test no-weak-ssl-ciphers
                 no-zlib no-zlib-dynamic no-idea no-mdc2 no-rc5 --prefix=${CMAKE_BINARY_DIR}
             BUILD_COMMAND make depend && make
             INSTALL_COMMAND make install_sw
-            BUILD_IN_SOURCE 1
-            DOWNLOAD_DIR "${DOWNLOAD_LOCATION}"
+            #BUILD_IN_SOURCE 1
+            DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
         )
     else ()
         ExternalProject_Add(openssl
-            PREFIX openssl
+            PREFIX ${CMAKE_CURRENT_BINARY_DIR}
             URL http://www.openssl.org/source/openssl-1.1.0h.tar.gz
             CONFIGURE_COMMAND perl Configure VC-WIN64A "--prefix=${CMAKE_INSTALL_PREFIX}"
             BUILD_COMMAND "ms\\do_win64a.bat"
             COMMAND nmake -f "ms\\ntdll.mak"
-            BUILD_IN_SOURCE 1
+            #BUILD_IN_SOURCE 1
             INSTALL_COMMAND nmake -f "ms\\ntdll.mak" install
         )
     endif ()
