751bffdd98717012118d6893f104548e47374f0d
Charly Lamothe <charly.lamothe@sfr.fr>
2018-07-09 12:18:05 +0200

    Check if openssl target is set in cmakelists.txt

--- external/openssl.cmake
+++ external/openssl.cmake
@@ -1,21 +1,23 @@
  ##########################################################################################
  # Copyright (C) 2018 by Charly Lamothe													  #
  #																						  #
- # This file is part of LibUnknownEcho.										  			  #
+ # This file is part of LibUnknownEchoCryptoModule.										  #
  #																						  #
- #   LibUnknownEcho is free software: you can redistribute it and/or modify   			  #
+ #   LibUnknownEchoCryptoModule is free software: you can redistribute it and/or modify   #
  #   it under the terms of the GNU General Public License as published by				  #
  #   the Free Software Foundation, either version 3 of the License, or					  #
  #   (at your option) any later version.												  #
  #																						  #
- #   LibUnknownEcho is distributed in the hope that it will be useful,        			  #
+ #   LibUnknownEchoCryptoModule is distributed in the hope that it will be useful,        #
  #   but WITHOUT ANY WARRANTY; without even the implied warranty of						  #
  #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the						  #
  #   GNU General Public License for more details.										  #
  #																						  #
  #   You should have received a copy of the GNU General Public License					  #
- #   along with LibUnknownEcho.  If not, see <http://www.gnu.org/licenses/>.  			  #
+ #   along with LibUnknownEchoCryptoModule.  If not, see <http://www.gnu.org/licenses/>.  #
  ##########################################################################################
+
+set(OPENSSL_TARGET_SET false)
 
 if (EXISTS ${CMAKE_CURRENT_BINARY_DIR}/openssl/src/openssl/libcrypto.a)
     set(OPENSSL_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl/src/openssl/)
@@ -55,4 +57,5 @@ else ()
         ${CMAKE_CURRENT_BINARY_DIR}/openssl/src/openssl/libcrypto.a
         ${CMAKE_CURRENT_BINARY_DIR}/openssl/src/openssl/libssl.a
     )
+    set(OPENSSL_TARGET_SET true)
 endif ()
